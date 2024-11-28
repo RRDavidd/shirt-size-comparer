112a340ee7b66b13ddfbbfc4d65d21890c45b4ff
Raphael David <raphaelruizd@gmail.com>
2024-11-28 21:43:09 +1100

    Change app title

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Layout from './components/Layout';
 import SizesCalculator from './components/SizeCalculator';
 
 function App() {
+
+  useEffect(() => {
+    document.title = "SizeMatch"
+  }, [])
+
   return (
     <div className="App">
       <Layout>
-        <h1 className="text-9xl pt-20 mb-14">Shirt Size Calculator</h1>
+        <h1 className="text-9xl pt-20 mb-14">SizeMatch</h1>
         <SizesCalculator />
       </Layout>
     </div>
